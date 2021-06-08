a67450ce61bb7b0b46fd1e6a3b7c458ba1779b78
Natalia Cheba <naticheb@onet.eu>
2021-06-08 22:38:34 +0200

    Added visits to doctor and patient classes

--- src/pl/take/clinic/Doctor.java
+++ src/pl/take/clinic/Doctor.java
@@ -1,6 +1,7 @@
 package pl.take.clinic;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Doctor {
@@ -16,6 +17,9 @@ public class Doctor {
 
     @Column(length = 45)
     private String speciality;
+
+    @Column()
+    private List<Visit> visits;
 
     public long getId() {
         return id;
@@ -43,5 +47,13 @@ public class Doctor {
 
     private void setSpeciality(String value) {
         this.speciality = value;
+    }
+
+    public String getVisits() {
+        return this.visits;
+    }
+
+    private void setVisits(List<Visit> value) {
+        this.visits = value;
     }
 }
