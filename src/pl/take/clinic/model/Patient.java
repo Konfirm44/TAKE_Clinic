378b63130bb0f5afa8951248f9eafbd703b01a38
Natalia Cheba <naticheb@onet.eu>
2021-06-12 21:50:27 +0200

    Added ids as autoincrement values

--- src/pl/take/clinic/model/Patient.java
+++ src/pl/take/clinic/model/Patient.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Entity
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(length = 45)
